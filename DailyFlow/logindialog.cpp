00d22a4d11861e19c16073f56ba8a7afa9247ddc
joco <joco9822@gmail.com>
2025-11-18 10:11:36 +0900

    :sparkles: [Feat] 로그인 다이얼로그 기능 구현 중...

--- DailyFlow/logindialog.cpp
+++ DailyFlow/logindialog.cpp
@@ -1,5 +1,6 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
+#include "joindialog.h"
 
 LoginDialog::LoginDialog(QWidget *parent) :
     QWidget(parent),
@@ -13,14 +14,29 @@ LoginDialog::~LoginDialog()
     delete ui;
 }
 
+void LoginDialog::setDataManager(DataManager *dataManager)
+{
+    m_dataManager = dataManager;
+}
+
 void LoginDialog::on_loginButton_clicked()
 {
 
 }
 
 
-void LoginDialog::on_joinButton_clicked()
+void LoginDialog::on_joinButton_clicked()  // Join 버튼을 클릭하면 회원가입 창이 열림
 {
+    // JoinDialog 객체 생성
+    JoinDialog *joinDialog = new JoinDialog(this);  // this를 인자로 넘김(LoginDialog가 닫힐 때 JoinDialog도 같이 닫함)
 
+    // 3. (핵심) LoginDialog가 갖고 있던 DataManager를 JoinDialog에게 넘겨줌
+    // (이래야 JoinDialog가 DB에 접근 가능)
+    joinDialog->setDataManager(m_dataManager);
+
+    // 4. JoinDialog 창을 띄움
+    // .show()는 비모달(Non-Modal): Join 창과 Login 창을 동시에 조작 가능
+    // .exec()는 모달(Modal): Join 창을 닫기 전까지 Login 창 조작 불가능
+    joinDialog->show();
 }
 
--- DailyFlow/logindialog.h
+++ DailyFlow/logindialog.h
@@ -1,8 +1,8 @@
 #ifndef LOGINDIALOG_H
 #define LOGINDIALOG_H
 
+#include "datamanager.h"
 #include <QWidget>
-#include <QMessageBox>
 
 namespace Ui {
 class LoginDialog;
@@ -16,13 +16,16 @@ public:
     explicit LoginDialog(QWidget *parent = nullptr);
     ~LoginDialog();
 
+    void setDataManager(DataManager *dataManager);
+
 private slots:
     void on_loginButton_clicked();
-
     void on_joinButton_clicked();
 
 private:
     Ui::LoginDialog *ui;
+
+    DataManager* m_dataManager;  // LoginDialog에서 DB에 접근하기 위함 (유저네임, 패스워드 확인 용도)
 };
 
 #endif // LOGINDIALOG_H
